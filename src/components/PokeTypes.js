5f567592c88fa25da1ec81ae746f9be8f3d14c3a
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2023-10-22 00:16:54 -0700

    Refactor.

--- src/components/PokeTypes.js
+++ src/components/PokeTypes.js
@@ -1,21 +1,25 @@
-import { Badge, BadgeText, HStack } from '@gluestack-ui/themed'
+import {
+  Badge,
+  BadgeText,
+  HStack
+} from '@gluestack-ui/themed'
 
-const PokeTypes = props => {
+const PokeTypes = ({ types = [], color = 'red' }) => {
   return (
     <HStack space='sm'>
-      { props.types.map((type, i) => (
+      { types.map((type, i) => (
         <Badge
           key={ i }
           px='$2' py='$1'
           variant='outline'
           size='sm'
           borderRadius='$full'
-          borderColor={ `$${props.color}600` }
+          borderColor={ `$${ color }600` }
           useSelect='none'
-          bgColor={ `$${props.color}100` }
+          bgColor={ `$${ color }100` }
         >
           <BadgeText
-            color={ `$${props.color}900` }
+            color={ `$${ color }900` }
             fontWeight='$bold'
           >
             { type.type.name }
