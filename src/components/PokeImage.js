465dc8331ca64ce224065f6cba6a5934b3747333
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2023-10-23 14:27:33 -0700

    Componente PokeImage añadido.

---
+++ src/components/PokeImage.js
@@ -0,0 +1,41 @@
+import {
+  Center,
+  Image,
+  View
+} from '@gluestack-ui/themed'
+import { useState } from 'react'
+
+const PokeImage = ({
+  color = 'red',
+  id = 1,
+  imageProps = {}
+}) => {
+  const [imageDimensions, setImageDimensions] = useState({ w: 0, h: 0 })
+
+  const onLayout = e => {
+    const { width, height } = e.nativeEvent.layout
+
+    setImageDimensions({ w: width, h: height })
+  }
+
+  return (
+    <Center>
+      <View
+        position='absolute'
+        w={ imageDimensions.w - 10 } h={ imageDimensions.h - 10 }
+        bgColor={ `$${ color }100` }
+        borderRadius='$full'
+      />
+      <Image
+        { ...imageProps }
+        onLayout={ onLayout }
+        source={{
+          uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
+        }}
+        alt='Pokemon image.'
+      />
+    </Center>
+  )
+}
+
+export default PokeImage
