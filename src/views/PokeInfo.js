6d1507aba60cd4776554024fdf4fc2099a79abc4
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2023-10-23 00:01:25 -0700

    AbilitySheet implementado.

--- src/views/PokeInfo.js
+++ src/views/PokeInfo.js
@@ -1,4 +1,8 @@
-import { ScrollView, VStack, View } from '@gluestack-ui/themed'
+import {
+  ScrollView,
+  VStack,
+  View
+} from '@gluestack-ui/themed'
 import { useSelector } from 'react-redux'
 
 import BasicDisplay from '../components/pokeInfo/BasicDisplay'
@@ -6,33 +10,34 @@ import SpeciesDisplay from '../components/pokeInfo/SpeciesDisplay'
 import AbilitiesDisplay from '../components/pokeInfo/AbilitiesDisplay'
 import SectionContainer from '../components/SectionContainer'
 import StatsDisplay from '../components/pokeInfo/StatsDisplay'
+import AbilitySheet from '../components/pokeInfo/AbilitySheet'
 
 const PokeInfo = () => {
   const pokemon = useSelector(state => state.pokemon)
   const { color } = pokemon
 
   return (
-    <VStack
-      bgColor={ `$${ color }100` }
-      h='100%'
-    >
-      <View p='$1'>
-        <BasicDisplay />
-      </View>
-      <ScrollView>
-        <VStack space='sm' p='$1'>
-          <SectionContainer heading='Species'>
-            <SpeciesDisplay />
-          </SectionContainer>
-          <SectionContainer heading='Abilities'>
-            <AbilitiesDisplay />
-          </SectionContainer>
-          <SectionContainer heading='Base Stats'>
-            <StatsDisplay  />
-          </SectionContainer>
-        </VStack>
-      </ScrollView>
-    </VStack>
+    <>
+      <VStack h='100%' bgColor={ `$${ color }100` }>
+        <View p='$1'>
+          <BasicDisplay />
+        </View>
+        <ScrollView>
+          <VStack space='sm' p='$1'>
+            <SectionContainer heading='Species'>
+              <SpeciesDisplay />
+            </SectionContainer>
+            <SectionContainer heading='Abilities'>
+              <AbilitiesDisplay />
+            </SectionContainer>
+            <SectionContainer heading='Base stats'>
+              <StatsDisplay  />
+            </SectionContainer>
+          </VStack>
+        </ScrollView>
+      </VStack>
+      <AbilitySheet />
+    </>
   )
 }
 
