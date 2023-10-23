a2733804b89826634040a10c689d5566d4232e5d
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2023-10-23 16:51:12 -0700

    Diseño actualizado.

--- src/components/GenCard.js
+++ src/components/GenCard.js
@@ -1,28 +1,77 @@
-import { Box, Pressable, Text } from '@gluestack-ui/themed'
+import {
+  Badge,
+  BadgeText,
+  HStack,
+  Heading,
+  Pressable,
+  VStack
+} from '@gluestack-ui/themed'
 import { useNavigation } from '@react-navigation/native'
 import { useDispatch } from 'react-redux'
 
 import { setGenerationId } from '../redux/generation'
+import PokeImage from './PokeImage'
 
-const GenCard = ({ name = 'Unknown Generation', id = 1 }) => {
+const GenCard = ({
+  name = 'Unknown generation',
+  id = 1,
+  pokemon = 'Unknown pokémon',
+  species = 0
+}) => {
   const navigation = useNavigation()
   const dispatch = useDispatch()
 
+  const onPress = () => {
+    dispatch(setGenerationId(id))
+    navigation.navigate('PokeList')
+  }
+
   return (
     <Pressable
-      onPress={ () => {
-        dispatch(setGenerationId(id))
-        navigation.navigate('PokeList')
-      } }
-      borderWidth='$2'
+      borderWidth='$1'
+      borderRadius='$md'
+      borderColor='$coolGray800'
+      overflow='hidden'
+      flexGrow={ 1 }
+      onPress={ onPress }
     >
-      {({ pressed }) => {
+      { ({ pressed, hovered }) => {
         return (
-          <Box p='$4' bgColor={ pressed ? '$indigo' : '$green' }>
-            <Text color='$white'>{ name }</Text>
-          </Box>
+          <HStack
+            space='sm'
+            bgColor={ `$coolGray${ pressed ? '400' : hovered ? '300' : '200' }` }
+            p='$4'
+          >
+            <PokeImage
+              imageProps={{ size: 'lg' }}
+              id={ pokemon }
+              color='coolGray'
+            />
+            <VStack space='xs' >
+              <Heading color='$coolGray800'>
+                { name }
+              </Heading>
+              <HStack>
+                <Badge
+                  px='$2' py='$1'
+                  variant='outline'
+                  size='sm'
+                  borderRadius='$full'
+                  borderColor='$coolGray600'
+                  bgColor='$coolGray100'
+                >
+                  <BadgeText
+                    color='$coolGray800'
+                    fontWeight='$bold'
+                  >
+                    { species } species
+                  </BadgeText>
+                </Badge>
+              </HStack>
+            </VStack>
+          </HStack>
         )
-      }}
+      } }
     </Pressable>
   )
 }
