57679661555ab27fa227f6fc6b2ea9334aa3085a
IvaanMiguel <mdominguez_20@alu.uabcs.mx>
2023-10-25 16:04:11 -0700

    Utils actualizado.

--- src/utils.js
+++ src/utils.js
@@ -8,5 +8,13 @@ export const getIdFromUrl = url => url.match(/\/(\d+)\/$/)[1]
  * @param {String} languageCode
  */
 export const getLocalizedString = (strings, languageCode = 'en') => {
-  return strings.find(str => str.language.name === languageCode)
+  return strings?.find(str => str.language.name === languageCode)
+}
+
+/**
+ * 
+ * @param {String} str
+ */
+export const formatName = str => {
+  return str.charAt(0).toUpperCase() + str.slice(1)
 }
